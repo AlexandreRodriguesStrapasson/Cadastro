363fabb6f22fd2ae82798d071f8f5791b382f67e
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-08-14 15:21:05 -0300

    Fit: start to develop the terminal response

--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputNome;
+    private EditText inputTelefone;
+    private CheckBox checkBox;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private EditText inputCidade;
+    private EditText inputUf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputNome = findViewById(R.id.inputNome);
+        inputTelefone = findViewById(R.id.inputTelefone);
+        checkBox = findViewById(R.id.checkBox);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        inputCidade = findViewById(R.id.inputCidade);
+        inputUf = findViewById(R.id.inputUf);
+
+        Button salvarBotao = findViewById(R.id.Bsalvar);
+        salvarBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
