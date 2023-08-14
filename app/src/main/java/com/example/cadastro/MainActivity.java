810cc8e02a5d65549029767f71c974c5a150143e
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-08-14 15:36:43 -0300

    Fit: Develop a operational button to save the info

--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -24,19 +24,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputNome = findViewById(R.id.inputNome);
-        inputTelefone = findViewById(R.id.inputTelefone);
-        checkBox = findViewById(R.id.checkBox);
-        radioButton1 = findViewById(R.id.radioButton1);
-        radioButton2 = findViewById(R.id.radioButton2);
-        inputCidade = findViewById(R.id.inputCidade);
-        inputUf = findViewById(R.id.inputUf);
+        inputNome       = findViewById(R.id.inputNome);
+        inputTelefone   = findViewById(R.id.inputTelefone);
+        checkBox        = findViewById(R.id.checkBox);
+        radioButton1    = findViewById(R.id.radioButton1);
+        radioButton2    = findViewById(R.id.radioButton2);
+        inputCidade     = findViewById(R.id.inputCidade);
+        inputUf         = findViewById(R.id.inputUf);
 
         Button salvarBotao = findViewById(R.id.Bsalvar);
         salvarBotao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String nome                 = inputNome.getText().toString();
+                String telefone             = inputTelefone.getText().toString();
+                Boolean respostaCheckBox    = checkBox.isChecked();
+                String sexo                 = radioButton1.isChecked() ? "Masculino" : "Feminino";
+                String cidade               = inputCidade.getText().toString();
+                String uf                   = inputUf.getText().toString();
 
+                System.out.println("Nome: " + nome);
+                System.out.println("Telefone" + telefone);
+                System.out.println("Ingressar na lista de E-mails: " + respostaCheckBox);
+                System.out.println("Sexo: " + sexo);
+                System.out.println("Cidade: " + cidade);
+                System.out.println("UF: " + uf);
             }
         });
 
