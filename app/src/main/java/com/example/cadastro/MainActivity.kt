2a3a1664f41208b27bb160d0603d3eedf129ae94
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-08-14 18:04:02 -0300

    feat: add a functional spinner and and the UF in the Toast

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cadastro
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cadastro.databinding.ActivityMainBinding
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         amd = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amd.main)
 
+        val estados = resources.getStringArray(R.array.estados_brasileiros)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, estados)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        amd.spinnerTv.adapter = adapter
+
         amd.btSave.setOnClickListener {
             val nomeCompleto = amd.nomeCompletoEt.text.toString()
             val telefone = amd.telefoneEt.text.toString()
@@ -22,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             val emailListado = if(amd.emailCb.isChecked) "Sim" else "Não"
             var sexo = "Não definido"
             val cidade = amd.cidadeEt.text.toString()
+            val uf = amd.spinnerTv.selectedItem.toString()
 
 
             if (amd.masculinoRb.isChecked){
@@ -38,7 +45,8 @@ class MainActivity : AppCompatActivity() {
                         "E-mail: $email \n" +
                         "Inscrito na lista de E-mails: $emailListado \n" +
                         "Sexo: $sexo \n" +
-                        "Cidade: $cidade",
+                        "Cidade: $cidade \n" +
+                        "UF: $uf",
                 Toast.LENGTH_SHORT
             ).show()
         }
