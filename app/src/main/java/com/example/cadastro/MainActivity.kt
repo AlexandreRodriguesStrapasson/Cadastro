2e849aedf8353a69e82faefa8cf76ae689762bd4
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2024-08-14 17:16:13 -0300

    feat: developed the initial Toast tests

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.cadastro
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amd: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        amd = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amd.main)
+
+        amd.btSave.setOnClickListener { v: View ->
+            val nomeCompleto = amd.nomeCompletoEt.text.toString()
+            val telefone = amd.telefoneEt.text.toString()
+
+            Toast.makeText(
+                this@MainActivity,
+                "Nome: $nomeCompleto \n" +
+                        "Telefone: $telefone",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 }
